8b00fa0e0fa3f73f0ac8decdde687926bebff3f1
JACK-0F-All-Trades <abdullah.25.sharif@gmail.com>
2022-10-01 11:03:49 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+
+import './App.css';
+import './index.css';
+import Navbar from './components/Navbar';
+
+import TextForm from './components/TextForm';
+
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+
+
+
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+ 
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.color = "white";
+      document.body.style.backgroundColor = "#001534";
+
+      showAlert("Dark mode enabled", "success");
+
+    }
+    else {
+      setMode("light");
+      document.body.style.color = "black";
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode enabled", "success");
+    }
+  }
+
+  return (
+    <>
+
+      <Navbar title="Welcome" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <TextForm showAlert={showAlert} heading="Enter Your Text Below" />
+
+    </>
+  );
+}
+
+export default App;
